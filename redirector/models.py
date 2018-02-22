729098214d5326a1ebcb6da21367719d6473d9fd
Shayne <shayne@nomad.ca>
2018-02-22 18:43:25 +0000

    Update models.py

--- redirector/models.py
+++ redirector/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from . import settings
+from redirector import settings
 
 
 class Redirect (models.Model):
